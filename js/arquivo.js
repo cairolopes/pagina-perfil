b9c3ed2cf1623064bf22071b46bd8153de133360
cairolopes <cairosilvalopes23@gmail.com>
2018-07-12 14:56:02 -0300

    maniputação do js

---
+++ js/arquivo.js
@@ -0,0 +1,11 @@
+function enviar() {
+
+  var nome = document.getElementById('nomeDigitado');
+
+  if(nome.value !=""){
+  alert("Hey, " + nome.value + " aqui é o Steven, estamos em uma aventura nao podemos responder agora, voltei mais tarde!");
+  }
+  else {
+    alert("Olá ninguem, aqui é o Steven, estamos em uma aventura nao podemos responder agora, voltei mais tarde! com um nome");
+  }
+}
